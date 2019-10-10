5d0d6e6074e1d6f4323795a8848760686316cf58
Zalyalova Yasina <zalyalova@infomatika.ru>
2019-10-10 04:02:27 +0300

    Initial commit

---
+++ presentation/src/main/java/ru/headsandhands/presentation/view/LoggedInUserView.java
@@ -0,0 +1,18 @@
+package ru.headsandhands.presentation.view;
+
+/**
+ * Class exposing authenticated user details to the UI.
+ */
+public class LoggedInUserView {
+
+    private String displayName;
+    //... other data fields that may be accessible to the UI
+
+    public LoggedInUserView(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+}
